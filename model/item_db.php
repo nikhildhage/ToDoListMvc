9988e8537dbfcd1c063f90eff8db4f77b64a8eee
nikhildhage <nikhil.dhage@gmail.com>
2024-04-28 21:19:13 -0500

    added get_items_by_category function to item_db.php

---
+++ model/item_db.php
@@ -0,0 +1,25 @@
+<?php
+function get_items_by_category($categoryID)
+{
+    global $db;
+    if ($categoryID) {
+        $query = 'SELECT t.ItemNum, t.Title, t.Description, c.categoryName FROM todoitems t
+                LEFT JOIN categories c ON t.categoryID = c.categoryID
+                WHERE t.categoryID = :categoryID 
+                ORDER BY t.ItemNum';
+    } else {
+        $query = 'SELECT t.ItemNum, t.Title, t.Description, c.categoryName FROM todoitems t
+                 LEFT JOIN categories c ON t.categoryID = c.categoryID
+                 ORDER BY t.ItemNum';
+    }
+    
+    $statement = $db->prepare($query);
+    if ($categoryID) {
+        $statement.bindValue(':categoryID', $categoryID);
+    }
+    $statement->execute();
+    $items = $statement->fetchAll();
+    $statement->closeCursor();
+    return $items;
+}
+
