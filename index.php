957ee0f0c8df37639ed1d1ecfeef1a032f2e508b
nikhildhage <nikhil.dhage@gmail.com>
2024-05-01 18:31:34 -0500

    Added switch case statements for actions: list categories and add categories

--- index.php
+++ index.php
@@ -15,4 +15,26 @@ $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT) ?: f
 // Determine the action to take, defaulting to 'list_items' if none specified
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?: filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?: 'list_items';
 
+switch ($action) {
+    case "list_categories":
+        $categories = get_categories();
+        include('view/category_list.php');
+        break; // Prevent fall-through
+    case "add_category":
+        if (!empty($category_name)) {
+            add_category($category_name);
+            header("Location: .?action=list_categories");
+            exit();
+        } else {
+            $error_message = "Invalid category name. Please check the field and try again.";
+            include("view/error.php");
+            exit();
+        }
+        break;
+    default:
+        $categories = get_categories();
+        $items = get_items_by_category($category_id);
+        include('view/item_list.php');
+        // No break needed after default as it's the last case
+}
 ?>
